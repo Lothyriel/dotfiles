244a9bdccbee13ac102a26a2e9429fc7e3b60d0d
fastjonh <fastjonh@gmail.com>
2023-08-17 19:07:51 -0300

    removing lsp rust config
    adding typescript lsp config

--- configs/lspconfig.lua
+++ configs/lspconfig.lua
@@ -1,18 +1,3 @@
-local on_attach = require("plugins.configs.lspconfig").on_attach
-local capabilities = require("plugins.configs.lspconfig").capabilities
 local lspconfig = require("lspconfig")
-local util = require "lspconfig/util"
 
-lspconfig.rust_analyzer.setup({
-    on_attach = on_attach,
-    capabilities = capabilities,
-    file_types = {"rust"},
-    root_dir = util.root_pattern("Cargo.toml"),
-    settings = {
-        ['rust-analyzer'] = {
-            cargo = {
-                allFeatures = true,
-            },
-        },
-    },
-})
+lspconfig.tsserver.setup {}
