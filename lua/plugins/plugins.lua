f27053a030e1fa875f8ab019775c007e58669aa9
fastjonh <fastjonh@gmail.com>
2023-08-28 23:56:54 -0300

    adding extra parsers and lsp

---
+++ lua/plugins/plugins.lua
@@ -0,0 +1,24 @@
+return {
+  -- add more treesitter parsers
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = {
+        "html",
+        "markdown",
+        "markdown_inline",
+        "yaml",
+      },
+    },
+  },
+
+  -- add any tools you want to have installed below
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "stylua",
+      },
+    },
+  },
+}
