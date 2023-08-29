95549b02d003a175a986d21f3475516f315346b5
fastjonh <fastjonh@gmail.com>
2023-08-29 11:02:13 -0300

    ja tinha com leader x x...

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,5 +1,3 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
-
-vim.keymap.set("n", "<leader>cD", "<cmd>TroubleToggle<cr>", { noremap = true, silent = true, desc = "Toggle trouble diagnostics" })
