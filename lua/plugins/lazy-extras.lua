d9a079730e453f024fba193e3537001d8a85afa7
fastjonh <fastjonh@gmail.com>
2023-08-28 23:52:55 -0300

    extracting extras to separate file

---
+++ lua/plugins/lazy-extras.lua
@@ -0,0 +1,9 @@
+return {
+  -- languages
+  { import = "lazyvim.plugins.extras.lang.typescript" },
+  { import = "lazyvim.plugins.extras.lang.rust" },
+  { import = "lazyvim.plugins.extras.lang.docker" },
+  { import = "lazyvim.plugins.extras.lang.json" },
+  --tools
+  { import = "lazyvim.plugins.extras.dap.core" },
+}
