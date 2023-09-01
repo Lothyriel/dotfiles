e1386cf09567ae462fd5da13d3fa7a85e6c71c60
fastjonh <fastjonh@gmail.com>
2023-09-01 02:18:42 -0300

    settings keybinds on the plugin file

--- lua/plugins/nvterm.lua
+++ lua/plugins/nvterm.lua
@@ -1,6 +1,13 @@
+local handler = function()
+  require("nvterm.terminal").toggle("horizontal")
+end
+
 return {
   "NvChad/nvterm",
   config = function()
     require("nvterm").setup()
   end,
+  keys = {
+    { "<leader>h", handler, desc = "Open terminal horizontally" },
+  },
 }
