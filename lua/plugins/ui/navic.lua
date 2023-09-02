d9e25f30e373767753e25ee6924ff26ecc13b5d6
fastjonh <fastjonh@gmail.com>
2023-09-02 05:42:50 -0300

    removing lazyvim functio call

--- lua/plugins/ui/navic.lua
+++ lua/plugins/ui/navic.lua
@@ -6,7 +6,7 @@ return {
   lazy = true,
   init = function()
     vim.g.navic_silence = true
-    require("lazyvim.util").on_attach(function(client, buffer)
+    require("utils").on_attach(function(client, buffer)
       if client.server_capabilities.documentSymbolProvider then
         require("nvim-navic").attach(client, buffer)
       end
