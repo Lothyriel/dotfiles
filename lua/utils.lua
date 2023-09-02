af57982eff4a99e382d5d51fde01a000a6083cc1
fastjonh <fastjonh@gmail.com>
2023-09-02 13:00:10 -0300

    fix

--- lua/utils.lua
+++ lua/utils.lua
@@ -3,8 +3,45 @@ local utils = require("lazy.core.util")
 local M = {}
 
 local terminals = {}
-
 local enabled = true
+
+function M.lsp_get_config(server)
+  local configs = require("lspconfig.configs")
+  return rawget(configs, server)
+end
+
+-- this will return a function that calls telescope.
+-- cwd will default to lazyvim.util.get_root
+-- for `files`, git_files or find_files will be chosen depending on .git
+function M.telescope(builtin, opts)
+  local params = { builtin = builtin, opts = opts }
+  return function()
+    builtin = params.builtin
+    opts = params.opts
+    opts = vim.tbl_deep_extend("force", { cwd = M.get_root() }, opts or {})
+    if builtin == "files" then
+      if vim.loop.fs_stat((opts.cwd or vim.loop.cwd()) .. "/.git") then
+        opts.show_untracked = true
+        builtin = "git_files"
+      else
+        builtin = "find_files"
+      end
+    end
+    if opts.cwd and opts.cwd ~= vim.loop.cwd() then
+      opts.attach_mappings = function(_, map)
+        map("i", "<a-c>", function()
+          local action_state = require("telescope.actions.state")
+          local line = action_state.get_current_line()
+          M.telescope(params.builtin, vim.tbl_deep_extend("force", {}, params.opts or {}, { cwd = false, default_text = line }))()
+        end)
+        return true
+      end
+    end
+
+    require("telescope.builtin")[builtin](opts)
+  end
+end
+
 function M.toggle_diagnostics()
   enabled = not enabled
   if enabled then
@@ -145,6 +182,8 @@ function M.toggle(option, silent, values)
     end
   end
 end
+
+M.root_patterns = { ".git", "lua" }
 
 -- returns the root directory based on:
 -- * lsp workspace folders
