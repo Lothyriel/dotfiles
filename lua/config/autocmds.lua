82311285929c54e97505787045a20ebaf0c26fc4
fastjonh <fastjonh@gmail.com>
2023-09-02 13:01:20 -0300

    descomentando um teste

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -17,14 +17,14 @@ vim.api.nvim_create_autocmd("TextYankPost", {
 })
 
 -- resize splits if window got resized
--- vim.api.nvim_create_autocmd({ "VimResized" }, {
---   group = augroup("resize_splits"),
---   callback = function()
---     local current_tab = vim.fn.tabpagenr()
---     vim.cmd("tabdo wincmd =")
---     vim.cmd("tabnext " .. current_tab)
---   end,
--- })
+vim.api.nvim_create_autocmd({ "VimResized" }, {
+  group = augroup("resize_splits"),
+  callback = function()
+    local current_tab = vim.fn.tabpagenr()
+    vim.cmd("tabdo wincmd =")
+    vim.cmd("tabnext " .. current_tab)
+  end,
+})
 
 -- go to last loc when opening a buffer
 vim.api.nvim_create_autocmd("BufReadPost", {
