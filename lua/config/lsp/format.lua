a97b7eac943b7d93b223ab35bf2191604bc4b829
fastjonh <fastjonh@gmail.com>
2023-09-02 14:15:16 -0300

    removing unused fn

--- lua/config/lsp/format.lua
+++ lua/config/lsp/format.lua
@@ -5,24 +5,6 @@ local M = {}
 ---@type PluginLspOpts
 M.opts = nil
 
-function M.enabled()
-  return M.opts.autoformat
-end
-
-function M.toggle()
-  if vim.b.autoformat == false then
-    vim.b.autoformat = nil
-    M.opts.autoformat = true
-  else
-    M.opts.autoformat = not M.opts.autoformat
-  end
-  if M.opts.autoformat then
-    Util.info("Enabled format on save", { title = "Format" })
-  else
-    Util.warn("Disabled format on save", { title = "Format" })
-  end
-end
-
 ---@param opts? {force?:boolean}
 function M.format(opts)
   local buf = vim.api.nvim_get_current_buf()
