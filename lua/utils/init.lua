d9b445c02c77bcb5a9eaddeabf821c0ee5c104ef
fastjonh <fastjonh@gmail.com>
2023-09-02 07:25:31 -0300

    adding opts fn

--- lua/utils/init.lua
+++ lua/utils/init.lua
@@ -4,6 +4,16 @@ local M = {}
 
 local terminals = {}
 
+---@param name string
+function M.opts(name)
+  local plugin = require("lazy.core.config").plugins[name]
+  if not plugin then
+    return {}
+  end
+  local Plugin = require("lazy.core.plugin")
+  return Plugin.values(plugin, "opts", false)
+end
+
 ---@param plugin string
 function M.has(plugin)
   return require("lazy.core.config").spec.plugins[plugin] ~= nil
