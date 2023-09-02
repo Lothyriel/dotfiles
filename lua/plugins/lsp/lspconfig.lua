b4169bfb94f532741b56684bbf5c431fa32ff315
fastjonh <fastjonh@gmail.com>
2023-09-02 07:28:46 -0300

    removing unused conditional dependency

--- lua/plugins/lsp/lspconfig.lua
+++ lua/plugins/lsp/lspconfig.lua
@@ -7,12 +7,7 @@ return {
     { "folke/neodev.nvim", opts = {} },
     "mason.nvim",
     "williamboman/mason-lspconfig.nvim",
-    {
-      "hrsh7th/cmp-nvim-lsp",
-      cond = function()
-        return require("utils").has("nvim-cmp")
-      end,
-    },
+    "hrsh7th/cmp-nvim-lsp",
   },
   ---@class PluginLspOpts
   opts = {
