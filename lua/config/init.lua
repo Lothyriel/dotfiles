c9074c1e0af3477a2b5302527c512f73c2b22946
fastjonh <fastjonh@gmail.com>
2023-09-02 09:46:36 -0300

    refac config.init to export function

--- lua/config/init.lua
+++ lua/config/init.lua
@@ -1,6 +1,31 @@
-require("config.options")
-require("config.autocmds")
-require("config.keymaps")
-require("config.lsp")
+local lazy = require("config.lazy")
 
-require("config.lazy")
+local load_configs = function()
+  local utils = require("utils")
+
+  if vim.fn.argc(-1) == 0 then
+    -- autocmds and keymaps can wait to load
+    vim.api.nvim_create_autocmd("User", {
+      group = vim.api.nvim_create_augroup("LazyVim", { clear = true }),
+      pattern = "VeryLazy",
+      callback = function()
+        utils.load("autocmds")
+        utils.load("keymaps")
+      end,
+    })
+  else
+    -- load them now so they affect the opened buffers
+    utils.load("autocmds")
+    utils.load("keymaps")
+  end
+end
+
+return {
+  init_lazy = function()
+    lazy.bootstrap()
+
+    load_configs()
+
+    lazy.init()
+  end,
+}
