db105d69a0d18eee9fff93631b36b419075006fe
fastjonh <fastjonh@gmail.com>
2023-09-04 20:05:34 -0300

    adding whickey desc

--- lua/plugins/editor/whichkey.lua
+++ lua/plugins/editor/whichkey.lua
@@ -15,6 +15,7 @@ return {
       ["<leader>b"] = { name = "+buffer" },
       ["<leader>c"] = { name = "+code" },
       ["<leader>f"] = { name = "+file/find" },
+      ["<leader>n"] = { name = "+noice" },
       ["<leader>g"] = { name = "+git" },
       ["<leader>gh"] = { name = "+hunks" },
       ["<leader>q"] = { name = "+quit/session" },
