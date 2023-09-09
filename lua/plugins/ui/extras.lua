2d9d1452f39fe70c608c5727db1d92369728adb9
fastjonh <fastjonh@gmail.com>
2023-09-09 05:20:02 -0300

    removing noice whickey

--- lua/plugins/ui/extras.lua
+++ lua/plugins/ui/extras.lua
@@ -1,11 +1,4 @@
 return {
-  {
-    -- noicer ui
-    "folke/which-key.nvim",
-    opts = function(_, opts)
-      opts.defaults["<leader>sn"] = { name = "+noice" }
-    end,
-  },
   -- icons
   { "nvim-tree/nvim-web-devicons", lazy = true },
 
