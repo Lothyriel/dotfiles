81811a7c1c5113733c21d624ebe92d4a905de4a4
fastjonh <fastjonh@gmail.com>
2023-09-09 03:40:53 -0300

    moving to utils

--- lua/config/init.lua
+++ lua/config/init.lua
@@ -19,20 +19,6 @@ local function get_plugin_tables(path, r_plugins)
   return plugins
 end
 
-local function map(mode, lhs, rhs, opts)
-  local keys = require("lazy.core.handler").handlers.keys
-  ---@cast keys LazyKeysHandler
-  -- do not create the keymap if a lazy keys handler exists
-  if not keys.active[keys.parse({ lhs, mode = mode }).id] then
-    opts = opts or {}
-    opts.silent = opts.silent ~= false
-    if opts.remap and not vim.g.vscode then
-      opts.remap = nil
-    end
-    vim.keymap.set(mode, lhs, rhs, opts)
-  end
-end
-
 local did_init = false
 
 local opts = {
@@ -96,7 +82,6 @@ local init = function()
 end
 
 return {
-  map = map,
   init_lazy = function()
     bootstrap()
 
--- lua/utils.lua
+++ lua/utils.lua
@@ -4,6 +4,20 @@ local M = {}
 
 local enabled = true
 
+function M.map(mode, lhs, rhs, opts)
+  local keys = require("lazy.core.handler").handlers.keys
+  ---@cast keys LazyKeysHandler
+  -- do not create the keymap if a lazy keys handler exists
+  if not keys.active[keys.parse({ lhs, mode = mode }).id] then
+    opts = opts or {}
+    opts.silent = opts.silent ~= false
+    if opts.remap and not vim.g.vscode then
+      opts.remap = nil
+    end
+    vim.keymap.set(mode, lhs, rhs, opts)
+  end
+end
+
 function M.lsp_get_config(server)
   local configs = require("lspconfig.configs")
   return rawget(configs, server)
