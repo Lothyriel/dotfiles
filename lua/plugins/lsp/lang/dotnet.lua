0eda40c92073fc3a6d95e2d7f5c6fa9182db332d
fastjonh <fastjonh@gmail.com>
2023-09-29 00:26:08 -0300

    fix ensure installed omnisharp

--- lua/plugins/lsp/lang/dotnet.lua
+++ lua/plugins/lsp/lang/dotnet.lua
@@ -1,8 +1,8 @@
 return {
-  "williamboman/mason.nvim",
-  opts = function(_, opts)
-    if type(opts.ensure_installed) == "table" then
-      vim.list_extend(opts.ensure_installed, { "omnisharp" })
-    end
-  end,
+  "neovim/nvim-lspconfig",
+  opts = {
+    servers = {
+      omnisharp = {},
+    },
+  },
 }
