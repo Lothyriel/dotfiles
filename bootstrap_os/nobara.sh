e897f2c4f5e20bfae4d2236c5f11eb09099fdd61
João Xavier <56160019+Lothyriel@users.noreply.github.com>
2024-07-14 04:24:41 -0300

    Update nobara.sh

--- bootstrap_os/nobara.sh
+++ bootstrap_os/nobara.sh
@@ -8,13 +8,15 @@ sudo dnf install -y cmake
 sudo dnf copr enable atim/lazygit -y
 sudo dnf install -y lazygit
 
-git config --global user.email "fastjonh@gmail.com"
-git config --global user.name "João Xavier"
-git clone https://github.com/Lothyriel/dotfiles ~/.config/nvim
+sudo dnf install opus-devel
 
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 
 mkdir $HOME/.config/alacritty
 wget -O $HOME/.config/alacritty/alacritty.toml https://raw.githubusercontent.com/Lothyriel/dotfiles/main/bootstrap_os/alacritty.toml
+
+git config --global user.email "fastjonh@gmail.com"
+git config --global user.name "João Xavier"
+git clone https://github.com/Lothyriel/dotfiles ~/.config/nvim
 
 gh auth login
