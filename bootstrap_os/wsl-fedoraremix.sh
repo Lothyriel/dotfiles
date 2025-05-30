be2b508033c4b9376745e6a238a7748bb0f87ca5
João Xavier <56160019+Lothyriel@users.noreply.github.com>
2025-05-30 17:53:05 -0300

    Update wsl-fedoraremix.sh

--- bootstrap_os/wsl-fedoraremix.sh
+++ bootstrap_os/wsl-fedoraremix.sh
@@ -14,11 +14,11 @@ rm lazygit
 rm lazygit.tar.gz
 
 # installing neovim tarball
-wget https://github.com/neovim/neovim/releases/download/v0.10.1/nvim-linux64.tar.gz
-tar xzvf nvim-linux64.tar.gz
-sudo mv nvim-linux64 /opt/nvim
+wget https://github.com/neovim/neovim/releases/download/v0.11.2/nvim-linux-x86_64.tar.gz
+tar xzvf nvim-linux-x86_64.tar.gz
+sudo mv nvim-linux-x86_64 /opt/nvim
 sudo ln -s /opt/nvim/bin/nvim /usr/local/bin/nvim
-rm nvim-linux64.tar.gz
+rm nvim-linux-x86_64.tar.gz
 
 # neovim dependencies
 sudo dnf install g++ -y
@@ -31,14 +31,15 @@ sudo dnf install cmake -y
 # neovim dotfiles
 git clone https://github.com/Lothyriel/dotfiles ~/.config/nvim
 
-# shell config
-sudo dnf install zsh -y
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-# change theme to 'agnoster'
+# fish install
+sudo dnf install fish -y
+curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source && fisher install jorgebucaran/fisher
+fisher install hauleth/agnoster
+agnoster powerline
 
 # rust
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
-echo 'export PATH="$HOME/.cargo/bin:$PATH"' >>.zshrc
+echo "set -gx PATH $HOME/.cargo/bin $PATH" >> ~/.config/fish/config.fish
 rustup component add rust-analyzer
 # wasm-pack
 curl https://rustwasm.github.io/wasm-pack/installer/init.sh -sSf | sh
@@ -48,9 +49,8 @@ sudo dnf install dotnet-sdk-8.0 -y
 
 # fly.io cli
 curl -L https://fly.io/install.sh | sh
-echo 'export FLYCTL_INSTALL="$HOME/.fly"' >>.zshrc
-echo 'export PATH="$FLYCTL_INSTALL/bin:$PATH"' >>.zshrc
-source ~/.zshrc
+echo "set -gx FLYCTL_INSTALL $HOME/.fly" >> ~/.config/fish/config.fish
+echo "set -gx PATH $FLYCTL_INSTALL/bin $PATH" >> ~/.config/fish/config.fish
 fly auth login
 
 # configure github auth
