441f08ec065c341f037c9c0313db00ded26af0c5
João Xavier <fastjonh@gmail.com>
2025-06-17 17:36:14 -0300

    configuring dap for dotnet

--- lua/plugins/dotnet.lua
+++ lua/plugins/dotnet.lua
@@ -10,6 +10,23 @@ vim.lsp.config("roslyn", {
   },
 })
 
+local function rebuild_project(co, path)
+  local spinner = require("easy-dotnet.ui-modules.spinner").new()
+  spinner:start_spinner("Building")
+  vim.fn.jobstart(string.format("dotnet build %s", path), {
+    on_exit = function(_, return_code)
+      if return_code == 0 then
+        spinner:stop_spinner("Built successfully")
+      else
+        spinner:stop_spinner("Build failed with exit code " .. return_code, vim.log.levels.ERROR)
+        error("Build failed")
+      end
+      coroutine.resume(co)
+    end,
+  })
+  coroutine.yield()
+end
+
 return {
   {
     "GustavEikaas/easy-dotnet.nvim",
@@ -23,6 +40,15 @@ return {
     opts = { ensure_installed = { "c_sharp" } },
   },
   {
+    "nvimtools/none-ls.nvim",
+    optional = true,
+    opts = function(_, opts)
+      local nls = require("null-ls")
+      opts.sources = opts.sources or {}
+      table.insert(opts.sources, nls.builtins.formatting.csharpier)
+    end,
+  },
+  {
     "seblyng/roslyn.nvim",
     ft = "cs",
     ---@module 'roslyn.config'
@@ -33,19 +59,10 @@ return {
   },
   {
     "mason-org/mason.nvim",
-    opts = { ensure_installed = { "csharpier", "netcoredbg" } },
+    opts = { ensure_installed = { "csharpier", "netcoredbg", "roslyn" } },
   },
   {
     "Issafalcon/neotest-dotnet",
-  },
-  {
-    "nvimtools/none-ls.nvim",
-    optional = true,
-    opts = function(_, opts)
-      local nls = require("null-ls")
-      opts.sources = opts.sources or {}
-      table.insert(opts.sources, nls.builtins.formatting.csharpier)
-    end,
   },
   {
     "stevearc/conform.nvim",
@@ -78,35 +95,88 @@ return {
   },
   {
     "mfussenegger/nvim-dap",
-    optional = true,
-    opts = function()
+    enabled = true,
+    config = function()
       local dap = require("dap")
-      if not dap.adapters["netcoredbg"] then
-        require("dap").adapters["netcoredbg"] = {
-          type = "executable",
-          command = vim.fn.exepath("netcoredbg"),
-          args = { "--interpreter=vscode" },
-          options = {
-            detached = false,
+      local dotnet = require("easy-dotnet")
+      local dapui = require("dapui")
+      dap.set_log_level("TRACE")
+
+      dap.listeners.before.attach.dapui_config = function()
+        dapui.open()
+      end
+      dap.listeners.before.launch.dapui_config = function()
+        dapui.open()
+      end
+      dap.listeners.before.event_terminated.dapui_config = function()
+        dapui.close()
+      end
+      dap.listeners.before.event_exited.dapui_config = function()
+        dapui.close()
+      end
+
+      local function file_exists(path)
+        local stat = vim.loop.fs_stat(path)
+        return stat and stat.type == "file"
+      end
+
+      local debug_dll = nil
+
+      local function ensure_dll()
+        if debug_dll ~= nil then
+          return debug_dll
+        end
+        local dll = dotnet.get_debug_dll()
+        debug_dll = dll
+        return dll
+      end
+
+      for _, value in ipairs({ "cs", "fsharp" }) do
+        dap.configurations[value] = {
+          {
+            type = "coreclr",
+            name = "Program",
+            request = "launch",
+            env = function()
+              local dll = ensure_dll()
+              local vars = dotnet.get_environment_variables(dll.project_name, dll.absolute_project_path)
+              return vars or nil
+            end,
+            program = function()
+              local dll = ensure_dll()
+              local co = coroutine.running()
+              rebuild_project(co, dll.project_path)
+              if not file_exists(dll.target_path) then
+                error("Project has not been built, path: " .. dll.target_path)
+              end
+              return dll.target_path
+            end,
+            cwd = function()
+              local dll = ensure_dll()
+              return dll.absolute_project_path
+            end,
           },
         }
-      end
-      for _, lang in ipairs({ "cs" }) do
-        if not dap.configurations[lang] then
-          dap.configurations[lang] = {
-            {
-              type = "netcoredbg",
-              name = "Launch file",
-              request = "launch",
-              ---@diagnostic disable-next-line: redundant-parameter
-              program = function()
-                return vim.fn.input("Path to dll: ", vim.fn.getcwd() .. "/", "file")
-              end,
-              cwd = "${workspaceFolder}",
-            },
-          }
+
+        dap.listeners.before["event_terminated"]["easy-dotnet"] = function()
+          debug_dll = nil
         end
+
+        dap.adapters.coreclr = {
+          type = "executable",
+          command = "netcoredbg",
+          args = { "--interpreter=vscode" },
+        }
       end
     end,
+    dependencies = {
+      { "nvim-neotest/nvim-nio" },
+      {
+        "rcarriga/nvim-dap-ui",
+        config = function()
+          require("dapui").setup()
+        end,
+      },
+    },
   },
 }
