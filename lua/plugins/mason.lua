be571adf3f917a10981f8230ad1d0daf5c7f414f
João Xavier <fastjonh@gmail.com>
2025-06-29 06:39:28 -0300

    added rust-analyzer to mason ensured

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -1,7 +1,7 @@
 return {
   "mason.nvim",
   opts = {
-    ensure_installed = { "html-lsp" },
+    ensure_installed = { "html-lsp", "rust-analyzer" },
     registries = {
       "github:mason-org/mason-registry",
       "github:Crashdummyy/mason-registry",
