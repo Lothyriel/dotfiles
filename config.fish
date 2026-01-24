6a49d2f22b2089506697fb495abbcf3d28dc8a82
João Xavier <fastjonh@gmail.com>
2026-01-24 19:27:30 -0300

    update config.fish

--- config.fish
+++ config.fish
@@ -9,6 +9,10 @@ alias ls="eza -lh --group-directories-first --icons=auto"
 alias lsa="ls -a"
 alias lt="eza --tree --level=2 --long --icons --git"
 alias lta="lt -a"
+
 zoxide init fish | source
-set -gx PATH /opt/nvim $PATH
+
 set -gx PATH $HOME/.fly/bin $PATH
+set -gr PATH $HOME/.local/bin $PATH
+set -gx PNPM_HOME "/home/jx/.local/share/pnpm"
+set -gx PATH "$PNPM_HOME" $PATH
