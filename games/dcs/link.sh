45d2c10339bbf1f6d4ffb592feaa03090472c703
João Xavier <fastjonh@gmail.com>
2026-02-23 09:55:06 -0300

    save dcs joysticks configs

---
+++ games/dcs/link.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env fish
+
+set dotfiles $HOME/repos/dotfiles
+
+ln -sf $dotfiles/games/dcs ~/Games/dcs-world/drive_c/users/steamuser/Saved\ Games/DCS/InputUserProfiles
---
+++ games/dcs/stick.diff.lua
@@ -0,0 +1,84 @@
+local diff = {
+	["axisDiffs"] = {
+		["a2101cdnil"] = {
+			["added"] = {
+				[1] = {
+					["filter"] = {
+						["curvature"] = {
+							[1] = 0,
+						},
+						["deadzone"] = 0.47,
+						["hardwareDetent"] = false,
+						["hardwareDetentAB"] = 0,
+						["hardwareDetentMax"] = 0,
+						["invert"] = false,
+						["saturationX"] = 1,
+						["saturationY"] = 1,
+						["slider"] = true,
+					},
+					["key"] = "JOY_RY",
+				},
+			},
+			["name"] = "Wheel Brake",
+		},
+	},
+	["keyDiffs"] = {
+		["d3010pnilu3010cd16vd1vpnilvu0"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN32",
+				},
+			},
+			["name"] = "Target Management Switch - Up",
+		},
+		["d3011pnilu3011cd16vd-1vpnilvu0"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN34",
+				},
+			},
+			["name"] = "Target Management Switch - Down",
+		},
+		["d3012pnilu3012cd16vd-1vpnilvu0"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN35",
+				},
+			},
+			["name"] = "Target Management Switch - Left",
+		},
+		["d3013pnilu3013cd16vd1vpnilvu0"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN33",
+				},
+			},
+			["name"] = "Target Management Switch - Right",
+		},
+		["d3020pnilu3020cd16vd1vpnilvu0"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN4",
+				},
+			},
+			["name"] = "CAMERA/GUN Trigger - FIRST DETENT",
+		},
+		["d3021pnilu3021cd16vd1vpnilvu0"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN5",
+				},
+			},
+			["name"] = "CAMERA/GUN Trigger - SECOND DETENT",
+		},
+		["d3022pnilu3022cd16vd1vpnilvu0"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN20",
+				},
+			},
+			["name"] = "WPN REL Button - Depress",
+		},
+	},
+}
+return diff
